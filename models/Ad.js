43d9b48ac159422352e7e536e6b45afb253f59a5
Eduardo <e.aboy@sinteac.es>
2017-09-16 18:56:00 +0200

    Creates ad model

---
+++ models/Ad.js
@@ -0,0 +1,39 @@
+'use stric';
+
+const mongoose = require('mongoose');
+
+// Schema definition
+
+const adSchema = mongoose.Schema({
+    name: {
+        type: String,
+        index: true 
+    },
+    onSell: {
+        type: Boolean,
+        index: true 
+    },
+    price: {
+        type: Number,
+        index: true 
+    },
+    image: String,
+    tags: {
+        type: [String],
+        index: true 
+    }
+});
+
+
+adSchema.statics.list = function(filter, limit){
+
+    const query = Ad.find(filter);
+    query.limit(limit);
+    return query.exec(); // Execute the query
+};
+
+// Create the model
+
+const Ad = mongoose.model('Agente', adSchema);
+
+module.exports = Ad;
